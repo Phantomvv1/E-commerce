01afee1fbbd84206fdb252008f50b5e0cab71d41
Phantomvv1 <krisi_andreev@abv.bg>
2025-04-07 17:59:25 +0300

    Added an endpoint for updating the items

--- internal/items/items.go
+++ internal/items/items.go
@@ -83,3 +83,83 @@ func CreateItem(c *gin.Context) {
 
 	c.JSON(http.StatusOK, nil)
 }
+
+func UpdateItem(c *gin.Context) { // needs testing
+	var information map[string]interface{}
+	json.NewDecoder(c.Request.Body).Decode(&information) // token && id && (name || desc)
+
+	token, ok := information["token"].(string)
+	if !ok {
+		log.Println("Incorrectly provided token")
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Error incorrectly provided token"})
+		return
+	}
+
+	_, accountType, err := ValidateJWT(token)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Error invalid token"})
+		return
+	}
+
+	if accountType != Admin {
+		c.JSON(http.StatusForbidden, gin.H{"error": "Error only admins can update items"})
+		return
+	}
+
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error unable to connect to the database"})
+		return
+	}
+	defer conn.Close(context.Background())
+
+	id, ok := information["id"].(float64)
+	if !ok {
+		log.Println("Incorrectly provided id")
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Error incorrectly provided id"})
+		return
+	}
+	itemID := int(id)
+
+	updateName := true
+	name, ok := information["name"].(string)
+	if !ok {
+		updateName = false
+	}
+
+	updateDesc := true
+	desc, ok := information["description"].(string)
+	if !ok && !updateName {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Error no information provided for the update"})
+		return
+	} else if !ok {
+		updateDesc = false
+	}
+
+	if updateName && updateDesc {
+		_, err = conn.Exec(context.Background(), "update e_commerce.items set name = $1, description = $2 where id = $3", name, desc, id)
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error unable to update the information in the database"})
+			return
+		}
+	} else if updateName {
+		_, err = conn.Exec(context.Background(), "update e_commerce.items set name = $1 where id = $2", name, id)
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error unable to update the information in the database"})
+			return
+		}
+	} else {
+		_, err = conn.Exec(context.Background(), "update e_commerce.items set description = $1 where id = $2", name, id)
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error unable to update the information in the database"})
+			return
+		}
+	}
+
+	c.JSON(http.StatusOK, nil)
+}

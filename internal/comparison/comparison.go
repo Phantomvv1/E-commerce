029f88f73c03c7ed40c4d338b3b77463bddc8d9f
Phantomvv1 <krisi_andreev@abv.bg>
2025-06-04 11:01:48 +0300

    Continueing with the comparisons

--- internal/comparison/comparison.go
+++ internal/comparison/comparison.go
@@ -3,6 +3,7 @@ package comparison
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -65,4 +66,66 @@ func AddItemToCompare(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, nil)
+}
+
+func Compare(c *gin.Context) {
+	var information map[string]string
+	json.NewDecoder(c.Request.Body).Decode(&information)
+
+	token, ok := information["token"]
+	if !ok {
+		log.Println("Incorrectly provided token")
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Error incorrectly provided token"})
+		return
+	}
+
+	id, _, err := ValidateJWT(token)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error invalid token"})
+		return
+	}
+
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error unable to coonect to the database"})
+		return
+	}
+	defer conn.Close(context.Background())
+
+	rows, err := conn.Query(context.Background(), "select itemID from e_commerce.comparison where userID = $1", id)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error unable to get the information about the items you want to compare from the database"})
+		return
+	}
+
+	itemIDs := []int{}
+	for rows.Next() {
+		itemID := 0
+		err = rows.Scan(&itemID)
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error unable to get the id-s of the items you want to compare"})
+			return
+		}
+
+		itemIDs = append(itemIDs, itemID)
+	}
+
+	if rows.Err() != nil {
+		log.Println(rows.Err())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error getting the id-s of the items"})
+		return
+	}
+
+	query := "select name, description, price from e_commerce.items where id in ("
+	for i := range len(itemIDs) {
+		if i == len(itemIDs)-1 {
+			query += "$" + fmt.Sprintf("%d", i+1)
+		} else {
+			query += "$" + fmt.Sprintf("%d", i+1) + ", "
+		}
+	}
 }
